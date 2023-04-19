faab176bb3d9ba42ddbdaf1a0ad76a75b832d40c
Clover <cloveryou02@163.com>
2023-04-19 23:04:51 +0800

    todo 新增分类

--- mall-product/src/main/java/top/ctong/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/top/ctong/mall/product/MallProductApplication.java
@@ -2,14 +2,19 @@ package top.ctong.mall.product;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import top.ctong.mall.common.RespBodyAdviceConfig;
 
 @MapperScan("top.ctong.mall.product.webApi.dao")
+@ImportAutoConfiguration(RespBodyAdviceConfig.class)
 @SpringBootApplication
 public class MallProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallProductApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MallProductApplication.class, args);
+        System.out.println("run");
     }
 
 }
--- mall-product/src/main/java/top/ctong/mall/product/webApi/category/CategoryController.java
+++ mall-product/src/main/java/top/ctong/mall/product/webApi/category/CategoryController.java
@@ -2,8 +2,10 @@ package top.ctong.mall.product.webApi.category;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import top.ctong.mall.common.models.CategoryTree;
+import top.ctong.mall.common.config.valid.UpdateGroup;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,22 +23,54 @@ import java.util.List;
 @RequestMapping("/category")
 public class CategoryController {
 
+    /**
+     * 日志
+     */
     private Logger log = LoggerFactory.getLogger(CategoryController.class);
 
+    /**
+     * 分类业务服务
+     */
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
+    /**
+     * 查询所有商品菜单
+     *
+     * @return List<CategoryTree>
+     * @author Clover You
+     * @date 2023/4/19 20:58
+     */
     @RequestMapping("/tree")
     public List<CategoryTree> tree() {
         return categoryService.listWithTree();
     }
 
+    /**
+     * 通过分类id删除分类
+     *
+     * @param catIds 分类id
+     * @author Clover You
+     * @date 2023/4/19 20:58
+     */
     @DeleteMapping("/delete")
     public void delete(@RequestBody Integer[] catIds) {
         categoryService.delete(Arrays.asList(catIds));
+    }
+
+    /**
+     * 新增分类
+     *
+     * @param dto 分类信息
+     * @author Clover You
+     * @date 2023/4/19 21:14
+     */
+    @PostMapping("/save")
+    public void save(@RequestBody @Validated({UpdateGroup.class}) CategorySaveDto dto) {
+
     }
 
 }
